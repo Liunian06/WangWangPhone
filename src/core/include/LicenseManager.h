c79cc44f955649032a4de129d5e612e40e8876fe
Liunian06 <277914121@qq.com>
2026-02-04 19:06:22 +0800

    feat: 注入 RSA 公钥至 C++ 核心层，完成离线授权系统

--- src/core/include/LicenseManager.h
+++ src/core/include/LicenseManager.h
@@ -30,7 +30,10 @@ public:
     std::string getMachineId();
 
 private:
-    LicenseManager() : activated(false) {}
+    LicenseManager() : activated(false) {
+        // 注入生成的 RSA 公钥 (SPKI 格式 Base64)
+        publicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEApcy/Am7F1r7KDKQqMpTO0jnKToPlj9KNIRcSCYXZ07mPbgxialV4snWzrFMANEC5wlZ0shUUaL+525eFSn3ZzphyrxC75Kyaw0nFeISvY6uNlZxooEu6DS51KU4w5XX0oGlEfSPrx5SsmWvous1Xf6jC3I+RVX+raNCbLH2rrbmk+phkXCTLxzzWUWxQgonN1/PLPQMeLqaaKBcxnZ4rJCw0frWCFr60NTDCSAAt0w7YxNdsaCvTIaIkh62Mdi6qbpG2Tmr7D3viZmE4heO4f5lxi+vXG5KIIKOMAvPXPs14itV8DI5EzJM2C7KUW7xejeUvx3CvVePo7BVBw/4dLwIDAQAB";
+    }
     std::string publicKey;
     bool activated;
     LicensePayload currentPayload;
